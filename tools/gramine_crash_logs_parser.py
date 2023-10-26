d0a9d0467eed09ff4c5e897483c65779c9c702af
Anjali Rai <anjali.rai@intel.com>
2023-10-26 14:42:38 +0530

    Creating a script for Gramine Master

--- tools/gramine_crash_logs_parser.py
+++ tools/gramine_crash_logs_parser.py
@@ -71,7 +71,7 @@ def check_and_copy_crashes(c_hash, desc, known_crashes, known_hashes):
         copy_files(c_hash, res, count)
     else:
         if known_crashes.values():
-            issue_list = [int(k_v.split("_")[1]) for k_c, k_v in known_crashes.items() if k_c != "lost connection to test machine\n"]
+            issue_list = [int(os.path.basename(k_v).split("_")[1]) for k_c, k_v in known_crashes.items() if k_c != "lost connection to test machine\n"]
             if not issue_list:
                 issue_count = "1"
             else:
